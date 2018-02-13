d228b6e5f8af9fad00fdf7b4ece4cc87764058e4
SwathiKanduri <30653555+SwathiKanduri@users.noreply.github.com>
2018-02-13 20:17:10 +0530

    adding userheader

---
+++ src/Components/UserHeader.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const UserHeader = () => (
+    <div className="row user-header">
+        <div className="col-sm-4">
+            Full Name
+        </div>
+        <div className="col-xs-4">
+            Class
+        </div>
+        <div className="col-sm-4">
+            School
+        </div>
+    </div>
+)
+
+export default UserHeader
